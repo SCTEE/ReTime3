a826000012ce97b23b818617c31a97e23281a888
San <sctee@deakin.edu.au>
2020-04-27 14:16:30 +1000

    innitial commit

---
+++ app/src/main/java/com/example/retime/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.retime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase db;
+    SQLiteOpenHelper openHelper;
+     Button _register_btn, _login_btn;
+     EditText _main_password, _main_username;
+     Cursor cursor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        openHelper = new DatabaseHelper(this);
+        db = openHelper.getReadableDatabase();
+
+        _register_btn = (Button)findViewById(R.id.reg_btn);
+        _main_username = (EditText)findViewById(R.id.user_reg);
+        _main_password = (EditText)findViewById(R.id.pass_reg);
+        _login_btn = (Button)findViewById(R.id.login_btn);
+
+        _register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, register.class);
+                startActivity(intent);
+            }
+        });
+
+        _login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = _main_username.getText().toString();
+                String password = _main_password.getText().toString();
+                //cursor = db.rawQuery("SELECT * FROM " +DatabaseHelper.TABLE_NAME+ "WHERE" + DatabaseHelper.COL_5 + DatabaseHelper.COL_4, new String[] {username, password});
+                cursor = db.rawQuery("SELECT * FROM " + DatabaseHelper.TABLE_NAME + " WHERE " + DatabaseHelper.COL_5 + " =? AND " + DatabaseHelper.COL_4 + " =? ", new String[] {username, password});
+                if (cursor!=null){
+                    if(cursor.getCount() > 0){
+                        Toast.makeText(getApplicationContext(), "login successfully", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_LONG).show();
+                    }
+                }
+
+               // Intent intent = new Intent(MainActivity.this, login.class);
+               // startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/retime/register.java
@@ -0,0 +1,67 @@
+package com.example.retime;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class register extends AppCompatActivity {
+    SQLiteOpenHelper openHelper;
+    SQLiteDatabase db;
+    Button _reg_btn, _login_btn;
+    EditText _user_reg, _fname_reg, _lname_reg, _pass_reg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+
+        openHelper = new DatabaseHelper(this);
+        _reg_btn = (Button)findViewById(R.id.reg_btn);
+        _user_reg = (EditText)findViewById(R.id.user_reg);
+        _fname_reg = (EditText)findViewById(R.id.fname_reg);
+        _lname_reg = (EditText)findViewById(R.id.lname_reg);
+        _pass_reg = (EditText)findViewById(R.id.pass_reg);
+        _login_btn = (Button)findViewById(R.id.login_btn);
+
+        _reg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db = openHelper.getWritableDatabase();
+                String fname = _fname_reg.getText().toString();
+                String lname = _lname_reg.getText().toString();
+                String username = _user_reg.getText().toString();
+                String password = _pass_reg.getText().toString();
+                insertdata(fname, lname, username, password);
+                Toast.makeText(getApplicationContext(), "register successfully", Toast.LENGTH_LONG).show();
+            }
+        });
+
+//        _login_btn.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View v) {
+////                //Intent intent = new Intent(MainActivity.this, login.class);
+////                //startActivity(intent);
+////            }
+////        });
+
+    }
+
+    public void insertdata (String fname, String lname, String username, String password) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DatabaseHelper.COL_2, fname);
+        contentValues.put(DatabaseHelper.COL_3, lname);
+        contentValues.put(DatabaseHelper.COL_4, password);
+        contentValues.put(DatabaseHelper.COL_5, username);
+        long id= db.insert(DatabaseHelper.TABLE_NAME, null, contentValues);
+    }
+
+
+}
