15b10d4c50481e1578af06ca8dc224aa17a0b6c2
San <sctee@deakin.edu.au>
2020-05-08 23:24:55 +1000

    test commit 2

--- app/src/main/java/com/example/retime/dummy.java
+++ app/src/main/java/com/example/retime/dummy.java
@@ -10,5 +10,6 @@ public class dummy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dummy);
+
     }
 }
