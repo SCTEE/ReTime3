8d0cd9d03d3726e85d5f35cde3d6cde73ed61322
San <sctee@deakin.edu.au>
2020-05-12 23:16:02 +1000

    implement calendar view and calendar task database

--- app/src/main/java/com/example/retime/CalendarTask.java
+++ app/src/main/java/com/example/retime/CalendarTask.java
@@ -1,12 +1,18 @@
 package com.example.retime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -20,16 +26,9 @@ import java.util.Locale;
 
 public class CalendarTask extends AppCompatActivity {
 
-    ImageButton Tasks, Next, Prev;
-    TextView currentdate;
-    GridView calendardate;
-    int maxdays = 31;
-    List<Date> date = new ArrayList<>();
-    List<Events> events = new ArrayList<>();
-
-    SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy", Locale.ENGLISH);
-    SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM", Locale.ENGLISH);
-    SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.ENGLISH);
+    CalendarView calendar;
+    SQLiteDatabase db;
+    SQLiteOpenHelper openHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +36,22 @@ public class CalendarTask extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.activity_calendartask);
+        openHelper = new CalendarDatabase(this);
+        db = openHelper.getWritableDatabase();
+        calendar = findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Log.d("blabla1", Integer.toString(year));
+                Log.d("blabla2", Integer.toString(month + 1));
+                Log.d("blabla3", Integer.toString(dayOfMonth));
+            }
+        });
+        Log.d("hihi", new SimpleDateFormat("ddMMyyyy").format(new Date(calendar.getDate())));
 
-        Tasks = findViewById(R.id.tasks);
-        Tasks.setOnClickListener(new TasksOnClickListener());
-        currentdate = findViewById(R.id.textView7);
-        currentdate.setText(dateFormat.format(Calendar.getInstance().getTime()));
     }
+
+
 
     private class TasksOnClickListener implements View.OnClickListener{
         @Override
