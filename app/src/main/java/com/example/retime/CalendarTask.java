618090c89082a3cb4cc575c606417178716f8432
San <sctee@deakin.edu.au>
2020-05-14 23:57:18 +1000

    update calendar task and retrieve calendar task from database

--- app/src/main/java/com/example/retime/CalendarTask.java
+++ app/src/main/java/com/example/retime/CalendarTask.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
@@ -19,6 +20,7 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -63,20 +65,41 @@ public class CalendarTask extends AppCompatActivity {
         _dayofmonth = new DecimalFormat("00").format(Integer.parseInt(todayarr[0]));
         _month = todayarr[1];
         _year = todayarr[2];
+
+        String todaydate = _dayofmonth.concat(_month).concat(_year);
+        Cursor data = db.rawQuery("Select * From " + CalendarDatabase.TABLE_NAME + " Where " + CalendarDatabase.COL_1 + " = " + todaydate, null);
+        if (data.getCount() > 0){
+            data.moveToFirst();
+            firsttasket.setText(data.getString(1));
+            firsttimeet.setText(data.getString(2));
+            secondtasket.setText(data.getString(3));
+            secondtimeet.setText(data.getString(4));
+        }
+
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 _year = Integer.toString(year);
                 _month = new DecimalFormat("00").format(month + 1);
                 _dayofmonth = Integer.toString(dayOfMonth);
-                Log.d("blabla", _dayofmonth);
-                Log.d("blabla1", _month);
-                Log.d("blabla2", _year);
+                firsttasket.setText("");
+                firsttimeet.setText("");
+                secondtasket.setText("");
+                secondtimeet.setText("");
+                String todaydate = _dayofmonth.concat(_month).concat(_year);
+                Cursor data = db.rawQuery("Select * From " + CalendarDatabase.TABLE_NAME + " Where " + CalendarDatabase.COL_1 + " = " + todaydate, null);
+                if (data.getCount() > 0){
+                    data.moveToFirst();
+                    firsttasket.setText(data.getString(1));
+                    firsttimeet.setText(data.getString(2));
+                    secondtasket.setText(data.getString(3));
+                    secondtimeet.setText(data.getString(4));
+                }
             }
         });
-        Log.d("blabla", _dayofmonth);
-        Log.d("blabla1", _month);
-        Log.d("blabla2", _year);
+//        Log.d("blabla", _dayofmonth);
+//        Log.d("blabla1", _month);
+//        Log.d("blabla2", _year);
     }
 
     public void insertdata (String day, String firsttask, String firsttime, String secondtask, String secondtime) {
@@ -86,7 +109,8 @@ public class CalendarTask extends AppCompatActivity {
         contentValues.put(CalendarDatabase.COL_3, firsttime);
         contentValues.put(CalendarDatabase.COL_4, secondtask);
         contentValues.put(CalendarDatabase.COL_5, secondtime);
-        long id = db.insert(CalendarDatabase.TABLE_NAME, null, contentValues);
+        long id = db.replace(CalendarDatabase.TABLE_NAME, null, contentValues);
+        Toast.makeText(this, "save successfully", Toast.LENGTH_LONG).show();
 
     }
 
@@ -106,7 +130,7 @@ public class CalendarTask extends AppCompatActivity {
     private class TasksOnClickListener implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(CalendarTask.this, home.class); 
+            Intent intent = new Intent(CalendarTask.this, home.class);
             startActivity(intent);
         }
     }
